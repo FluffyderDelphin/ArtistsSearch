ee72b5de8a92d134471baf997b4b569e22a21a91
Alexander Urlich <ulrichalex12@yahoo.de>
2022-09-28 10:48:21 +0200

    setting up mongoose Shema

---
+++ src/models/Artist.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// setting up Mongoose Shema, Id will be generated when posted to the Database
+const ArtistsShema = mongoose.Schema({
+  name: String,
+  url: String,
+  image_small: String,
+  image: String,
+});
+
+module.exports = mongoose.model('Artists', ArtistShema);
