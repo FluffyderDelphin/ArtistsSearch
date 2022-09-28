b9a2d65a08e8a47ca8cd56b1805a8d9ddb9f6e73
Alexander Urlich <ulrichalex12@yahoo.de>
2022-09-28 12:26:22 +0200

    handling null return

--- src/routes/search.js
+++ src/routes/search.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const converter = require('json-2-csv');
+const path = require('path');
 
 const Artist = require('../models/Artist');
 
@@ -9,11 +10,17 @@ router.get('/:artistName', async (req, res) => {
   const param = req.params.artistName;
   try {
     const artist = await Artist.findOne({ name: param });
-
-    //converting json to csv
-    const csv = await converter.json2csvAsync(artist);
-    fs.writeFileSync('src/data/csv/data.csv', csv);
-    res.json(artist);
+    if (!artist) {
+      // if the mongose shema returns null, return the local Json List
+      const jsonList = fs.readFileSync(path.resolve('src/data/json/data.json'));
+      const list = JSON.parse(jsonList);
+      res.json(list.artistsList);
+    } else {
+      //converting json to csv
+      const csv = await converter.json2csvAsync(artist);
+      fs.writeFileSync('src/data/csv/data.csv', csv);
+      res.json(artist);
+    }
   } catch (err) {
     console.log(err);
   }
