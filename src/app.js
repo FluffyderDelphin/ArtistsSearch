c1d2ab02c226f3d3846f20c61788872bd890075c
Alexander Urlich <ulrichalex12@yahoo.de>
2022-09-28 10:33:06 +0200

    setting up Local Connection

--- src/app.js
+++ src/app.js
@@ -2,5 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 // to hide the database conection password in a local .env File
 require('dotenv/config');
+
+mongoose.connect(process.env.DB_CONNECTION, console.log('connected to DB'));
+
+const artistSearch = require('./routes/search');
+
+//runs localy on localhost:8080
+app.listen(8080);
