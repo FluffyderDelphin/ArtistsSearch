dc071846db6a78827101161a79f6a97bcf889cbb
Alexander Urlich <ulrichalex12@yahoo.de>
2022-09-28 11:05:54 +0200

    enabling to post to Database

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  console.log('Route Works');
+const Artist = require('../models/Artist');
+
+router.post('/', async (req, res) => {
+  const data = req.body;
+
+  //  creating new Artist from the Request Body
+  const artist = new Artist({
+    name: data.name,
+    url: data.url,
+    image_small: data.image_small,
+    image: data.image,
+  });
+
+  // saving Artist to Database
+  try {
+    const saveArtist = await artist.save();
+    res.json(saveArtist);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
