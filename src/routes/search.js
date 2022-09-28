5844b677bf18288e596b02d207617dc7ae4a8ca5
Alexander Urlich <ulrichalex12@yahoo.de>
2022-09-28 12:03:06 +0200

    creating csv file as artist.search response

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const converter = require('json-2-csv');
 
-router.get('/', (req, res) => {
-  console.log('route works');
+const Artist = require('../models/Artist');
+
+router.get('/:artistName', async (req, res) => {
+  const param = req.params.artistName;
+  try {
+    const artist = await Artist.findOne({ name: param });
+
+    //converting json to csv
+    const csv = await converter.json2csvAsync(artist);
+    fs.writeFileSync('src/data/csv/data.csv', csv);
+    res.json(artist);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
